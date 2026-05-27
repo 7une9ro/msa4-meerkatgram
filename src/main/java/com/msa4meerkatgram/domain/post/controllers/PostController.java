bc10dc19e4c5748c30ad0f9162600fcc97ef2cd8
7une9ro <racoon121238@gmail.com>
2026-05-27 17:45:09 +0900

    게시글 목록 페이징 조회 API 구현

---
+++ src/main/java/com/msa4meerkatgram/domain/post/controllers/PostController.java
@@ -0,0 +1,32 @@
+package com.msa4meerkatgram.domain.post.controllers;
+
+import com.msa4meerkatgram.domain.post.requests.PostIndexRequest;
+import com.msa4meerkatgram.domain.post.responses.PostIndexResponse;
+import com.msa4meerkatgram.domain.post.services.PostService;
+import com.msa4meerkatgram.global.responses.BaseResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping("/posts")
+    public ResponseEntity<BaseResponse<PostIndexResponse>> index(@Valid PostIndexRequest postIndexRequest) {
+
+        return ResponseEntity.status(200).body(
+                BaseResponse.<PostIndexResponse>builder()
+                        .code("00")
+                        .message("정상 처리")
+                        .data(postService.index(postIndexRequest))
+                        .build()
+        );
+    }
+}
---
+++ src/main/java/com/msa4meerkatgram/domain/post/services/PostService.java
@@ -0,0 +1,36 @@
+package com.msa4meerkatgram.domain.post.services;
+
+import com.msa4meerkatgram.domain.post.entities.Post;
+import com.msa4meerkatgram.domain.post.mapper.PostMapper;
+import com.msa4meerkatgram.domain.post.requests.PostIndexRequest;
+import com.msa4meerkatgram.domain.post.responses.PostIndexResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostMapper postMapper;
+
+    public PostIndexResponse index(PostIndexRequest postIndexRequest) {
+
+        int offset = (postIndexRequest.page() - 1) * postIndexRequest.limit();
+
+        // 특정 페이지 게시글 조회
+        List<Post> posts = postMapper.getPagination(postIndexRequest.limit(), offset);
+
+        // 전체 게시글 조회
+        long total = postMapper.getTotalPosts();
+        boolean lastPage = offset + postIndexRequest.limit() >= total;
+
+        // 컨트롤러 전달
+        return PostIndexResponse.builder()
+                .total(total)
+                .lastPage(lastPage)
+                .posts(posts)
+                .build();
+    }
+}
