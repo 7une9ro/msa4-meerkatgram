91b0be32a286589f4c9761749072f658dd9df5d9
7une9ro <racoon121238@gmail.com>
2026-05-27 17:45:21 +0900

    전역 예외 처리

---
+++ src/main/java/com/msa4meerkatgram/global/errors/GlobalExceptionHandler.java
@@ -0,0 +1,70 @@
+package com.msa4meerkatgram.global.errors;
+
+import com.msa4meerkatgram.global.errors.custom.NotRegisteredException;
+import com.msa4meerkatgram.global.responses.BaseResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotRegisteredException.class)
+    public ResponseEntity<BaseResponse<String>> notRegisteredHandle(NotRegisteredException e) {
+        return ResponseEntity.status(400).body(
+                BaseResponse.<String>builder()
+                        .code("E01")
+                        .message("로그인 에러")
+                        .data(e.getMessage())
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<BaseResponse<String>> methodArgumentTypeMismatchHandle(MethodArgumentTypeMismatchException e) {
+        return ResponseEntity.status(400).body(
+                BaseResponse.<String>builder()
+                        .code("E21")
+                        .message("요청 파라미터에 이상이 있습니다.")
+                        .data(String.format("%s : 필드를 확인해 주세요.", e.getName()))
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<BaseResponse<List<String>>> methodArgumentNotValidHandle(MethodArgumentNotValidException e) {
+        return ResponseEntity.status(400).body(
+                BaseResponse.<List<String>>builder()
+                        .code("E21")
+                        .message("요청 파라미터에 이상이 있습니다.")
+                        .data(
+                                e.getBindingResult()
+                                    .getAllErrors()
+                                    .stream()
+                                    .map(item -> String.format("%s : 잘못된 값입니다.", item.getObjectName()))
+                                    .toList()
+                        )
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<BaseResponse<String>> othersHandle(Exception e) {
+        log.error("시스템 에러: {}\n{}", e.getMessage(), Arrays.toString(e.getStackTrace()));
+
+        return ResponseEntity.status(500).body(
+                BaseResponse.<String>builder()
+                        .code("E99")
+                        .message("시스템 에러")
+                        .data("현재 서비스 이용이 불가능합니다. 잠시 후 다시 시도해 주십시오.")
+                        .build()
+        );
+    }
+}
