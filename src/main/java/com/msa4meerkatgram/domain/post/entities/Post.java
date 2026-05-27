85c4400ba22e4af62cdae45116210554859f4dfe
7une9ro <racoon121238@gmail.com>
2026-05-27 17:44:09 +0900

    Post 엔티티 및 Post 조회 요청 DTO 객체

---
+++ src/main/java/com/msa4meerkatgram/domain/post/entities/Post.java
@@ -0,0 +1,20 @@
+package com.msa4meerkatgram.domain.post.entities;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class Post {
+    private Long id;
+    private Long userId;
+    private String content;
+    private String image;
+    private String createdAt;
+    private String updatedAt;
+    private String deletedAt;
+
+    // private LocalDateTime createdAt;
+    // private LocalDateTime updatedAt;
+    // private LocalDateTime deletedAt;
+}
